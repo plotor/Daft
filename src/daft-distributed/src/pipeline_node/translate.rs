02654fa8cf25cee807a6f2b4b1d7b32b05f869fe
plotor <zhenchao.wang@hotmail.com>
2025-08-07 19:54:17 +0800

    feat: Add offset support to DataFrame & SQL
    Signed-off-by: plotor <zhenchao.wang@hotmail.com>

--- src/daft-distributed/src/pipeline_node/translate.rs
+++ src/daft-distributed/src/pipeline_node/translate.rs
@@ -198,19 +198,15 @@ impl TreeNodeVisitor for LogicalPlanToPipelineNodeTranslator {
                 )
                 .arced()
             }
-            LogicalPlan::Limit(limit) => {
-                if limit.offset.is_some() {
-                    todo!("FLOTILLA_MS3: Implement Offset")
-                }
-                Arc::new(LimitNode::new(
-                    self.get_next_pipeline_node_id(),
-                    logical_node_id,
-                    &self.stage_config,
-                    limit.limit as usize,
-                    node.schema(),
-                    self.curr_node.pop().unwrap(),
-                ))
-            }
+            LogicalPlan::Limit(limit) => Arc::new(LimitNode::new(
+                self.get_next_pipeline_node_id(),
+                logical_node_id,
+                &self.stage_config,
+                limit.limit as usize,
+                limit.offset.map(|x| x as usize),
+                node.schema(),
+                self.curr_node.pop().unwrap(),
+            )),
             LogicalPlan::Project(project) => {
                 let projection = BoundExpr::bind_all(&project.projection, &project.input.schema())?;
                 ProjectNode::new(
@@ -447,10 +443,6 @@ impl TreeNodeVisitor for LogicalPlanToPipelineNodeTranslator {
             LogicalPlan::TopN(top_n) => {
                 let sort_by = BoundExpr::bind_all(&top_n.sort_by, &top_n.input.schema())?;
 
-                if top_n.offset.is_some() {
-                    todo!("FLOTILLA_MS3: Implement Offset")
-                }
-
                 // First stage: Perform a local topN
                 let local_topn = TopNNode::new(
                     self.get_next_pipeline_node_id(),
@@ -459,7 +451,8 @@ impl TreeNodeVisitor for LogicalPlanToPipelineNodeTranslator {
                     sort_by.clone(),
                     top_n.descending.clone(),
                     top_n.nulls_first.clone(),
-                    top_n.limit,
+                    top_n.limit + top_n.offset.unwrap_or(0),
+                    Some(0),
                     top_n.input.schema(),
                     self.curr_node.pop().unwrap(),
                 )
@@ -477,6 +470,7 @@ impl TreeNodeVisitor for LogicalPlanToPipelineNodeTranslator {
                     top_n.descending.clone(),
                     top_n.nulls_first.clone(),
                     top_n.limit,
+                    top_n.offset,
                     top_n.input.schema(),
                     gather,
                 )
